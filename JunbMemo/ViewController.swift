ad9e98c180df498d60a6543989ba6931cff38be4
junbeom-jeon <junbeom.jeon@navercorp.com>
2020-12-20 21:34:19 +0900

    Initial Commit

---
+++ JunbMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JunbMemo
+//
+//  Created by USER on 2020/12/20.
+//  Copyright © 2020 junb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
